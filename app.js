e5e8b99418e4f033aa8cdf09f0218c791ff2569e
Hasan Kaya <hasankaya3546@hotmail.com>
2023-05-24 16:25:51 +0300

    data from database shown on index page

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -24,9 +29,24 @@ app.use(express.json());
 app.use(myLogger2); */
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
+
+const photoId = '646e0013920c8537cff337dc';
+Photo.findByIdAndUpdate(photoId, {
+  title: 'Photo Title 3',
+  description: 'Photo description 3 lorem ipsum ',
+})
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/about', (req, res) => {
   res.render('about');
@@ -35,8 +55,8 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
   res.redirect('/');
 });
 
